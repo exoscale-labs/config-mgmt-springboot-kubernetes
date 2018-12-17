6a9c0b745f98cdbf84cb0ba9914a7f7b3e85189d
Nicolas Frankel <nicolas.frankel@exoscale.com>
2018-12-17 11:35:20 +0100

    Implement Spring profiles with Environment injection

--- src/main/java/com/exoscale/configmgmt/demo/WelcomeController.java
+++ src/main/java/com/exoscale/configmgmt/demo/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.exoscale.configmgmt.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WelcomeController {
 
+    private final Environment env;
+
+    public WelcomeController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/")
     public String welcome(Model model) {
+        model.addAttribute("label", env.getProperty("app.env.label"));
+        model.addAttribute("color", env.getProperty("app.env.color"));
         return "welcome";
     }
 }
